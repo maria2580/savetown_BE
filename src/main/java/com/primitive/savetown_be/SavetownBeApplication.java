022de705e8dbd2f7c50dc361aead12243b725202
maria2580 <marin6670@gmail.com>
2025-08-18 17:26:12 +0900

    Initial commit

---
+++ src/main/java/com/primitive/savetown_be/SavetownBeApplication.java
@@ -0,0 +1,13 @@
+package com.primitive.savetown_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavetownBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavetownBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/primitive/savetown_be/SavetownBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.primitive.savetown_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavetownBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
